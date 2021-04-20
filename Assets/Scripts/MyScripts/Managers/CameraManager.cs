1584a4390d5bdf6624844cfff7a997e8bc4345f6
leesm002 <gmlsenfn34@naver.com>
2021-04-20 13:49:08 +0900

    map added not completed

--- Assets/Scripts/MyScripts/Managers/CameraManager.cs
+++ Assets/Scripts/MyScripts/Managers/CameraManager.cs
@@ -6,11 +6,15 @@ public class CameraManager : MonoBehaviour
 {
     Vector3 pose;
 
+    Vector3 PreviousMousePos;
+    Vector3 CurrentMousePos;
+
     const float cameraPositionY = 1.95f;
     const float cameraPositionZ = -2.0f;
+    const float CAMERA_MOVEMENT_SPEED = 0.4f;
 
     Quaternion qt = new Quaternion(18.423f, 0, 0, 180);
-
+    
 
     private void Update()
     {
@@ -22,8 +26,29 @@ public class CameraManager : MonoBehaviour
         Camera.main.transform.position = pose;
         Camera.main.transform.rotation = qt;
 
+        if (Input.GetKeyDown(KeyCode.Mouse1))       // 마우스 우클릭 하는 순간
+        {
+            PreviousMousePos = Input.mousePosition;
+            Debug.Log($"{PreviousMousePos}");
+        }
+        if (Input.GetMouseButton(1))                // 마우스 우클릭 누르고있는 동안
+        {
+            CurrentMousePos = Input.mousePosition;
+
+            if (PreviousMousePos.x != CurrentMousePos.x)
+            {
+                if (PreviousMousePos.x < CurrentMousePos.x)
+                    transform.Rotate((PreviousMousePos.y - CurrentMousePos.y) * CAMERA_MOVEMENT_SPEED, (CurrentMousePos.x - PreviousMousePos.x) * CAMERA_MOVEMENT_SPEED, transform.rotation.z);
+                    //qt.y += ( PreviousMousePos.x - CurrentMousePos.x ) * 0.01f;
+
+                if (PreviousMousePos.x > CurrentMousePos.x)
+                    transform.Rotate((PreviousMousePos.y - CurrentMousePos.y) * CAMERA_MOVEMENT_SPEED, (CurrentMousePos.x - PreviousMousePos.x) * CAMERA_MOVEMENT_SPEED, transform.rotation.z);
+                //qt.y -= (CurrentMousePos.x - PreviousMousePos.x) * 0.01f;
+            }
+        }
     }
 
     
+
 
 }
