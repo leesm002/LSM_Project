b31300cdfbb1f2f99adcae0952383a8c84b3c80a
leesm002 <gmlsenfn34@naver.com>
2021-05-06 13:34:57 +0900

    not working bcuz UI not completed

--- Assets/Scripts/MyScripts/Managers/InputManager.cs
+++ Assets/Scripts/MyScripts/Managers/InputManager.cs
@@ -2,13 +2,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class InputManager
 {
     public Action KeyAction = null;
 
+
     public void OnUpdate()
     {
+        if (EventSystem.current.IsPointerOverGameObject())
+	        return;
+
         if (KeyAction != null)
         {
             KeyAction.Invoke();
---
+++ Assets/Scripts/MyScripts/Scene/Event/Shopper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shopper : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        //상점 UI 오픈
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //상점 UI 닫기
+    }
+}
