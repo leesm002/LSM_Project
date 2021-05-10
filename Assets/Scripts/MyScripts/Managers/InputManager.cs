095544251c0b575974d1d2bc938fe0442536fc9b
이상민 <gmlsenfn34@naver.com>
2021-05-10 12:03:24 +0900

    Assets Added

--- Assets/Scripts/MyScripts/Managers/InputManager.cs
+++ Assets/Scripts/MyScripts/Managers/InputManager.cs
@@ -11,8 +11,10 @@ public class InputManager
 
     public void OnUpdate()
     {
-        if (EventSystem.current.IsPointerOverGameObject())
+        /*
+        if (EventSystem.current.IsPointerOverGameObject())      //버튼 눌렀을 때 마우스 클릭으로 인한 캐릭터 이동을 방지하는 코드
 	        return;
+        */
 
         if (KeyAction != null)
         {
