6f972c2ce470d13d688199d4feef75f005122e3c
이상민 <gmlsenfn34@naver.com>
2021-05-11 13:15:50 +0900

    Combat Animator Conditions Added

---
+++ Assets/Scripts/MyScripts/Player/SD_Player_Combat_Controller.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SD_Player_Combat_Controller : MonoBehaviour
+{
+    private CharacterController controller;
+
+    float f_speed = 5.0f;
+
+    const float f_gravity = 9.8f;
+
+
+    private Animator anim;
+    private AnimatorStateInfo currentState;
+}
