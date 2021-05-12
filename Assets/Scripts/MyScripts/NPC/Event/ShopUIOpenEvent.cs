4f437129dbe6888a88c5c284d9480e5231864340
이상민 <gmlsenfn34@naver.com>
2021-05-12 13:23:46 +0900

    Shopper Animation Added

---
+++ Assets/Scripts/MyScripts/NPC/Event/ShopUIOpenEvent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopUIOpenEvent : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject UIManager = Managers.GetUIManager.Root;
+        Debug.Log("Collider Trigger");
+
+    }
+}
