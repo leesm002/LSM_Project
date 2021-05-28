2377628b40e41100c6f61bf91435a85f173687c4
이상민 <gmlsenfn34@naver.com>
2021-05-28 13:44:37 +0900

    HP_Bar Added

---
+++ Assets/Scripts/MyScripts/Combat/CombatUI/HP_BarController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HP_BarController : MonoBehaviour
+{
+    GameObject sdPlayer;
+    
+    void Start()
+    {
+        this.sdPlayer = GameObject.FindGameObjectWithTag("Player");
+        Debug.Log(sdPlayer);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sdPlayer != null)
+        {
+            transform.position = sdPlayer.transform.position;
+            transform.position += new Vector3(0f,1.4f,0f);
+        }
+        transform.rotation = Camera.main.transform.rotation;
+    }
+}
